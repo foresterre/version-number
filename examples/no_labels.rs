67785febb2eb5908ea2e7c3888b342fabae52ae0
Martijn <garm@ilumeo.com>
2024-04-25 19:12:43 +0200

    refactor: Restructure examples

--- examples/no_labels.rs
+++ examples/no_labels.rs
@@ -4,5 +4,5 @@ fn main() {
     // Additional labels such as build flags are not supported!
     let err = Version::parse("1.0.0-alpha").unwrap_err();
 
-    eprintln!("{err}"); // Expected end of input, but got '-' at 5.
+    eprintln!("{err}"); // prints: Expected end of input, but got '-' at 5.
 }
